c4033e85535889574cfb826b04841a4543e6268f
Abhay Rana <abhay.rana@healthmug.com>
2022-04-10 23:55:54 +0530

    setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./style/App.css";
+const App = () => {
+	return (
+		<>
+			<div>Abhay Rana</div>
+			<p className="text">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam itaque aperiam enim quisquam, modi accusantium ullam autem iure. Voluptates dolorum numquam eum deserunt
+				ipsam iusto, provident ipsum saepe voluptas voluptatibus facilis aliquam praesentium odio autem ea voluptatum magni dicta possimus ullam aspernatur illum amet, ipsa nulla. Maiores eum
+				quasi autem facilis in dicta possimus tempora? Exercitationem officiis sequi eos mollitia, nam deleniti ullam necessitatibus incidunt deserunt consectetur, nobis assumenda in facilis
+				neque vero. Labore magnam blanditiis tenetur vel quae quos veniam odio alias doloremque quibusdam facere dolor, velit soluta, saepe, nesciunt ab quidem quam! Ullam eaque provident
+				omnis beatae!
+			</p>
+			<p className="diff-text">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ullam culpa cupiditate illum laboriosam excepturi ea quaerat quam voluptates facilis, consectetur quisquam porro
+				obcaecati distinctio nostrum similique earum et quasi dolor quidem eius quas itaque! Iste sit officia quia labore sunt cum ratione commodi architecto repellat, earum fugiat atque, quam
+				error corporis laborum itaque officiis rem animi. Beatae omnis debitis neque repellendus a quibusdam laudantium, veniam velit? Cum nisi eius hic enim nemo, doloremque accusantium aut
+				culpa atque provident facere praesentium ullam est placeat error reprehenderit, totam, itaque nulla ex tempora distinctio optio. Dicta nemo ullam labore! Delectus nemo assumenda maxime
+				nobis commodi ea neque minus quos, dicta similique optio possimus libero provident excepturi ex facilis obcaecati sed temporibus hic dolorem architecto deleniti itaque ducimus eaque!
+				Inventore quasi officia consectetur labore vel dolorem perspiciatis porro temporibus dolor nobis quisquam, ipsam alias saepe itaque obcaecati deleniti blanditiis repellendus et,
+				architecto optio.
+			</p>
+		</>
+	);
+};
 
 export default App;
